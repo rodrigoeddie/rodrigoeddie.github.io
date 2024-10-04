9df4c67f1cb9b38f5605d1f9b39ce02844d48a9e
Rodrigo de Melo Lima <rodrigoeddie@gmail.com>
2024-10-04 19:20:53 +0000

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxt/content',
-    ],
+    modules: ['@nuxt/content', '@nuxt/image'],
 
     css: ["@/assets/styles/main.scss"],
     vite: {
