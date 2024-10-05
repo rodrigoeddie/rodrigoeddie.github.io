c4d5acd1c68b70cbeb10b16f04336e50e6a2b841
Rodrigo de Melo Lima <rodrigoeddie@gmail.com>
2024-10-05 00:45:16 +0000

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxt/content', '@nuxt/image'],
+  modules: ['@nuxt/content', '@nuxt/image'],
+  css: ["@/assets/styles/main.scss"],
 
-    css: ["@/assets/styles/main.scss"],
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: '@use "@/assets/styles/_variables.scss" as *;',
+  vite: {
+      css: {
+          preprocessorOptions: {
+              scss: {
+                  additionalData: '@use "@/assets/styles/_variables.scss" as *;',
 
-                },
-            },
-        },
-    },
+              },
+          },
+      },
+  },
+
+  compatibilityDate: '2024-10-05',
 })
